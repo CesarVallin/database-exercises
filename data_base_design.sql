5f7cf83283b117a2b52412a4ea2a49f213d6c175
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-08-23 16:57:32 -0500

    done with data_base_design

--- data_base_design.sql
+++ data_base_design.sql
@@ -3,10 +3,10 @@ SHOW DATABASES;
 
 USE database_design_db;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ads_categories;
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS ads_categories;
+DROP TABLE IF EXISTS users;
 
 
 CREATE TABLE users
@@ -23,7 +23,8 @@ CREATE TABLE ads (
     title VARCHAR(150) NOT NULL,
     description TEXT NOT NULL,
     users_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_id) REFERENCES users(id)
 )
 ;
 
@@ -42,25 +43,47 @@ CREATE TABLE ads_categories (
 )
 ;
 
+
+
 INSERT INTO categories (category_name) VALUES
                                            ('help wanted'),
                                            ('giveaway'),
-                                           ('furniture');
+                                           ('furniture'),
+                                           ('for sale'),
+                                           ('jobs'),
+                                           ('services'),
+                                           ('event'),
+                                           ('appliances');
 
 INSERT INTO users(email, user_name, password) VALUES
                                                   ('ava@email', 'ava_smith', 'ava1234'),
                                                   ('jesse@email', 'jesse_dorman', 'jesse1234'),
-                                                  ('cesar@email', 'cesar_vallin', 'cesar1234');
+                                                  ('cesar@email', 'cesar_vallin', 'cesar1234'),
+                                                  ('steve@email', 'steve_pflug', 'steve1234'),
+                                                  ('sandy@email', 'sandy_cheeks', 'sandy1234'),
+                                                  ('patrick@email', 'patrik_star', 'patrick1234'),
+                                                  ('sponge@email', 'sponge_bob', 'sponge1234'),
+                                                  ('kurt@email', 'skrt_skrt_kurt', 'kurt1234');
 
 INSERT INTO ads(title, description, users_id) VALUES
                                                   ('Help wanted - professional jar opener', 'I struggle daily opening these dang jars, only if god could bestow upon me a strong and abled individual', 1),
                                                   ('Old sock - heavily used', 'tired of those fresh out the package pair of socks? Annoyed with pulling matched pairs out of your drier? look no further, I have the sock for you!', 2),
-                                                  ('Giving away a chair!', 'Great chair, please steal me', 3);
+                                                  ('Giving away a chair!', 'Great chair, please steal me', 3),
+                                                  ('Country Music Festival!', 'come on down to the Krusty Krab for a night of fun and everything Texas!', 5),
+                                                  ('Fry Cook Worker', 'hard worker with years of experience in the fast food world. Ask my boss, Mr. Krabs', 7),
+                                                  ('Giving away freezer', 'Great freezer, but doesn\'t freeze', 4);
 INSERT INTO ads_categories(ads_id, categories_id) VALUES
                                                       (1, 1),
+                                                      (1, 6),
                                                       (2, 2),
+                                                      (2, 4),
                                                       (3, 2),
-                                                      (3, 3);
+                                                      (3, 3),
+                                                      (4, 7),
+                                                      (5, 5),
+                                                      (5, 6),
+                                                      (6, 2),
+                                                      (6, 8);
 
 SHOW TABLES;
 
@@ -113,5 +136,4 @@ WHERE users_id IN (
     )
 ;
 
-
-
+SHOW TABLES;
